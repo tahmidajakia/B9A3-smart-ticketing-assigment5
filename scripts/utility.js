14bb2e28a0047eaa3d2afe89cfbfdb976ba4b4b0
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-02-19 09:45:36 -0800

    creating javascript funtion

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,16 +1,58 @@
 const allBtn = document.getElementsByClassName("add-btn");
 let seat = 40;
 let seatCount = 0;
+// let totalCost = 0;
+// let mainCost = 550;
+
+
 
 for (const btn of allBtn) {
     btn.addEventListener("click", function (e) {
         seat = seat - 1;
-        document.getElementById("total-seat").innerText = seat;
-        seatCount = seatCount + 1;
-        document.getElementById("seat-count").innerText = seatCount;
+        const seatName = e.target.innerText;
+        const selectedSeat = document.getElementById("selected-seat");
         btn.classList.add('bg-[#1DD100]');
 
-        
+        const li = document.createElement("li");
+        li.style.display = "flex";
+        li.style.justifyContent = "space-between";
 
+        const p = document.createElement("p");
+        p.innerText = seatName;
+
+
+        const p2 = document.createElement("p");
+        p2.innerText = "Economy";
+
+
+        const p3 = document.createElement("p");
+        p3.innerText = 550;
+
+
+
+        selectedSeat.appendChild(li);
+        li.appendChild(p);
+        li.appendChild(p2);
+        li.appendChild(p3);
+
+
+        const price = 550;
+        const totalCost = document.getElementById("total-cost").innerText;
+
+        const convertedTotalCost=parseInt(totalCost);
+        document.getElementById("total-cost").innerText = convertedTotalCost + parseInt(price);
+
+
+        
+        setInnerText("total-seat", seat);
+        seatCount = seatCount + 1;
+        setInnerText("seat-count", seatCount);
     });
 }
+
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+    document.getElementById(id).innerText = value;
+
+}
+
