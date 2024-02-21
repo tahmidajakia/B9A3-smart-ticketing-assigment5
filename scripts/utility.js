3746ea7797fbf5d50043a32bad19ead25e943367
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-02-21 07:54:15 -0800

    created site with javascriptd

--- scripts/utility.js
+++ scripts/utility.js
@@ -11,7 +11,7 @@ for (const btn of allBtn) {
         seat = seat - 1;
         const seatName = e.target.innerText;
         const selectedSeat = document.getElementById("selected-seat");
-        btn.classList.add('bg-[#1DD100]');
+        
 
         const li = document.createElement("li");
         li.style.display = "flex";
@@ -39,20 +39,107 @@ for (const btn of allBtn) {
         const price = 550;
         const totalCost = document.getElementById("total-cost").innerText;
 
-        const convertedTotalCost=parseInt(totalCost);
-        document.getElementById("total-cost").innerText = convertedTotalCost + parseInt(price);
+        const convertedTotalCost = parseInt(totalCost);
+        const cost = convertedTotalCost + parseInt(price);
+        setInnerText("total-cost", cost);
 
 
-        
+
+        e.target.setAttribute("disabled", false);
+        e.target.style.background = "#1DD100";
+
+
+
+
+        // const bdtCost = cost;
+        // setInnerText("bdt-cost", bdtCost);
+
+
         setInnerText("total-seat", seat);
         seatCount = seatCount + 1;
         setInnerText("seat-count", seatCount);
+
+
+
+
+        const applyBtn = document.getElementById("apply-btn");
+        applyBtn.addEventListener("click", function () {
+            const inputField = document.getElementById("input-field").value;
+            const couponCode = inputField.split(" ").join("");
+
+
+            if (seatCount >= 4) {
+                if (couponCode === "NEW15") {
+
+                } else {
+                    alert("Invalid Coupon");
+                }
+
+            } else {
+                alert("Please buy at least 4 tickests!");
+            }
+
+
+        });
+
+
+
+
+
+
     });
+
+
 }
 
 function setInnerText(id, value) {
     document.getElementById(id).innerText = value;
     document.getElementById(id).innerText = value;
+    document.getElementById(id).innerText = value;
 
 }
+
+
+
+function hideElementById(id){
+    const element = document.getElementById(id);
+    element.classList.add('hidden');
+
+}
+function showElementById(id){
+    const element = document.getElementById(id);
+    element.classList.remove('hidden');
+
+}
+function next(){
+    hideElementById("header-section")
+    hideElementById("section-first")
+    hideElementById("section-second")
+    hideElementById("footer-screen")
+    showElementById("modal-section")
+
+}
+
+// function play(){
+//     const headerScreen = document.getElementById("header-section");
+//     headerScreen.classList.add('hidden');
+    
+// }
+// function play(){
+//     const sectionFirst = document.getElementById("section-first");
+//     sectionFirst.classList.add('hidden');
+    
+// }
+// function play(){
+//     const homeSection = document.getElementById("home-screen");
+//     homeSection.classList.add('hidden');
+    
+// }
+// function play(){
+//     const footerSection = document.getElementById("footer-screen");
+//     footerSection.classList.add('hidden');
+    
+// }
+
+
 
